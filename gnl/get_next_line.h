724d92fd5be7974aaec110742ccdb054ab6352f6
resource777 <79133019+resource777@users.noreply.github.com>
2022-01-31 17:51:30 +0900

    Create get_next_line.h

---
+++ gnl/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaewonki <jaewonki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 15:21:16 by jaewonki          #+#    #+#             */
+/*   Updated: 2022/01/31 15:21:16 by jaewonki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char			*read_file(int fd, char *keep);
+char			*get_line(char *keep);
+char			*get_next_line(int fd);
+
+char			*ft_strdup(const char *s1);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strjoin(char const *s1, char const *s2);
+size_t			ft_strlen(const char *s);
+size_t			ft_strlcpy(char *dst, const char *src, size_t size);
+
+#endif
